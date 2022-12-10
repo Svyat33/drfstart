c67cd600844d9e879bae49516b4d95a8db5307d4
Sviatoslav Kosenchuk <sviatoslav.kosenchuk@globallogic.com>
2022-12-10 11:08:43 +0200

    add news response

--- frontend/views.py
+++ frontend/views.py
@@ -1,10 +1,28 @@
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from frontend.models import News
+
 
 # Create your views here.
 def news_list(request):
-    return JsonResponse({"ok": True})
+
+    ret = []
+
+    for n in News.objects.all():
+        ret.append({
+            "id": n.id,
+            "title": n.title.capitalize(),
+            "anons": n.anons
+        })
+
+    return JsonResponse({"ok": True, "data": ret})
 
 
 def news_one(request, news_id):
-    return JsonResponse({"success": True})
+    n = get_object_or_404(News, pk=news_id)
+    return JsonResponse({"success": True, "data": {
+            "id": n.id,
+            "title": n.title.capitalize(),
+            "anons": n.anons
+        }})
